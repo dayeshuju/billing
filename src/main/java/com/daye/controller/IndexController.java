eb1ed321be35bc4308a5866a6ed3b820f10020f8
yangshuai12345 <1811578151@qq.com>
2019-06-18 14:02:24 +0800

    测试controller

---
+++ src/main/java/com/daye/controller/IndexController.java
@@ -0,0 +1,21 @@
+package com.daye.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping(value="/index",method= RequestMethod.GET)
+public class IndexController {
+    @RequestMapping(value="",method=RequestMethod.GET)
+    public String index(HttpServletRequest request, Model model){
+        List<Object> glfls=new ArrayList<Object>();
+        model.addAttribute("gnfls",glfls);
+        return "index";
+    }
+}
